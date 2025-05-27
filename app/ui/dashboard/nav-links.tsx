458f14f74f4b63fce166641c0c6a1398afb0308a
codehass <hassan.elouardy06@gmail.com>
2025-05-27 13:03:22 +0100

    fetch data inside nav link component

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,7 +23,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log('Current pathname:', pathname);
+
   return (
     <>
       {links.map((link) => {
