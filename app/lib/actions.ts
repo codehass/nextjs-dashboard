78aa071d65dc6edacf4c12322c071a7492161f45
codehass <hassan.elouardy06@gmail.com>
2025-05-31 13:40:35 +0100

    delete error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -109,7 +109,6 @@ export async function updateInvoice(id: string, prevState: State,formData: FormD
 };
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   await sql`
     DELETE FROM invoices
     WHERE id = ${id}
